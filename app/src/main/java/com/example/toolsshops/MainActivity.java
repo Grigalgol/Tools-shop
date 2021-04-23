12933cf192eaf13f732e5792ebb94ddf11182910
Grigoriy <grisha101202@yandex,ru>
2021-04-23 16:23:12 +0300

    init android project

---
+++ app/src/main/java/com/example/toolsshops/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.toolsshops;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
